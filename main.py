735ff59f4bb998164662f51b3744847e8362a118
cupertank <ilyavologin@gmail.com>
2019-08-24 18:58:02 +0300

    First

---
+++ main.py
@@ -0,0 +1,31 @@
+from telegram import Bot, Update, Sticker
+from telegram.ext import Updater, MessageHandler
+from telegram.ext import Filters
+from random import randint
+import os
+
+my_sticker_set = None
+
+
+def qwerty_handler(bot: Bot, update: Update):
+    global my_sticker_set
+
+    numStick = randint(0, len(my_sticker_set.stickers))
+    bot.send_sticker(update.message.chat_id, my_sticker_set.stickers[numStick], reply_to_message_id=update.message.message_id)
+
+
+token = os.environ.get("token")
+updater = Updater(token)
+dispatcher = updater.dispatcher
+bot = updater.bot
+
+handlers = [
+    MessageHandler(Filters.user(username='memsofanime'), qwerty_handler)
+]
+
+for handler in handlers:
+    dispatcher.add_handler(handler)
+
+my_sticker_set = bot.get_sticker_set("QwertyNahui")
+
+updater.start_polling()
